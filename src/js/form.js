759ebb7d3d4beb2405f20096fa5d2c6112dc4cbb
Petro Shevchuk <shevchukmagnumpetro@gmail.com>
2017-02-05 12:00:52 +0200

    form fix

--- src/js/form.js
+++ src/js/form.js
@@ -1,11 +1,18 @@
-var inputs = document.querySelectorAll('.call-back-form__input');
+(function() {
+  var inputs = document.querySelectorAll('.call-back-form__input');
 
-for (var i = 0; i < inputs.length; i++) {
-  inputs[i].onfocus = function() {
-    this.classList.add('call-back-form__input--focus')
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].onfocus = function() {
+      var contain = this.classList.contains('call-back-form__input--focus');
+      if (contain !== true) {
+        this.classList.add('call-back-form__input--focus');
+      } 
+    }
+
+    inputs[i].onblur = function() {
+      if (this.value == "") {
+        this.classList.remove('call-back-form__input--focus');
+      }
+    }
   }
-  
-  inputs[i].onblur = function() {
-    this.classList.remove('call-back-form__input--focus')
-  }
-}
+})()
